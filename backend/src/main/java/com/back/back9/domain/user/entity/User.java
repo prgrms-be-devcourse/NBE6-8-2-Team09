7807429d91beaadf7cdd0af4f5857cb5108a0cb6
GarakChoi <tpghks312@naver.com>
2025-07-24 16:27:31 +0900

    add Login

--- backend/src/main/java/com/back/back9/domain/user/entity/User.java
+++ backend/src/main/java/com/back/back9/domain/user/entity/User.java
@@ -1,15 +1,25 @@
 package com.back.back9.domain.user.entity;
 
 import com.back.back9.global.jpa.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
-@Table(name = "users", indexes = {
-        @Index(name = "idx_users_username", columnList = "username")
-})
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class User extends BaseEntity {
@@ -27,11 +37,39 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
+    @Column(unique = true)
+    private String apiKey;
+
+    public User(String userLoginId, String username, String password) {
+        this.userLoginId = userLoginId;
+        this.username = username;
+        this.password = password;
+        this.role = UserRole.MEMBER;
+        this.apiKey = UUID.randomUUID().toString();
+    }
+
     public enum UserRole {
         MEMBER, ADMIN
     }
 
-    public void updatePassword(String password) {
-        this.password = password;
+    public void modifyApiKey(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    public boolean isAdmin() {
+        return role == UserRole.ADMIN;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<String> roles = new ArrayList<>();
+        if (isAdmin()) {
+            roles.add("ROLE_ADMIN");
+        } else {
+            roles.add("ROLE_MEMBER");
+        }
+
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .toList();
     }
 }
