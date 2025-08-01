2ce15b13a3802070258833578bcb074bfcb30302
lcs9317 <l65783082@gmail.com>
2025-08-01 11:41:39 +0900

    props fix3

--- frontend/src/app/(protected)/user/[id]/page.tsx
+++ frontend/src/app/(protected)/user/[id]/page.tsx
@@ -1,11 +1,12 @@
 import { getUser } from '@/lib/api/user'
 import type { User } from '@/types/user'
-import type { PageProps } from 'next'
 
-type Params = { id: string }
+interface UserPageProps {
+    params: Promise<{ id: string }>
+}
 
-export default async function UserPage({ params }: PageProps<Params>) {
-    const { id } = await params        // params가 Promise<Params>이므로 await
+export default async function UserPage({ params }: UserPageProps) {
+    const { id } = await params
     const user: User = await getUser(id)
 
     return (
